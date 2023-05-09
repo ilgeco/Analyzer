2c487eb329bf807c438d374cc28036e3aa252878
ilgeco <gabri-_-95@hotmail.it>
2023-05-09 17:36:57 +0200

    generic cmp impl

--- src/mode/handle_cmp.rs
+++ src/mode/handle_cmp.rs
@@ -9,37 +9,55 @@ use super::required_op;
 
 
 
+fn cmp_impl<'a, It, Num>(iter: It, series: impl Series<String, Num>) -> HashMap<&'a String, Option<(Num, Num)>>
+where
+    It: Iterator<Item = (&'a String, &'a Vec<Num>)>,
+    Num : required_op::Operation<'a> + 'a
+{
+    let mut res = HashMap::with_capacity(100);
+
+    for (name, value) in iter {
+        if let Some(cmp_elems) = series.get_serie(name) {
+            let mut tot_err = Num::zero();
+            let mut tot_rel = Num::zero();
+            let mut size_e = 0;
+            let mut size_r = 0;
+
+            for (&base, &cmp) in value.iter().zip(cmp_elems.iter()) {
+                tot_err =  base.sub(cmp).abs();
+
+                if base != Num::zero() {
+                    tot_rel = tot_rel + ((cmp - base) / base).abs();
+                    size_r += 1;
+                }
+                size_e += 1;
+            }
+            if size_r != 0 && size_e != 0 {
+                res.insert(name, Some(( tot_rel.divu(size_r), tot_err.divu(size_e))));
+
+            }else{
+                res.insert(name, None);
+            }
+        }
+    }
+    res
+}
+
 
 
 pub fn handle_cmp(file1: &Path, file2: &Path) {
     let f_list = parse(&retrive_file(file1), IdNomListBuilder);
     let s_list = parse(&retrive_file(file2), IdNomListBuilder);
 
-    for (name, base_elems) in f_list.into_iter() {
-        if let Some(cmp_elems) = s_list.get_serie(name) {
-            let mut tot_err = 0f64;
-            let mut tot_rel = 0f64;
-            let mut size_e = 0;
-            let mut size_r = 0;
 
-            for (&base, &cmp) in base_elems.iter().zip(cmp_elems.iter()) {
-                tot_err += base.sub(cmp).abs();
+    let res = cmp_impl(f_list.into_iter(), s_list);
 
-                if base != 0f64 {
-                    tot_rel += ((cmp - base) / base).abs();
-                    size_r += 1;
-                }
-                size_e += 1;
-            }
-            if size_r != 0 && size_e != 0 {
-                println!(
-                    "{}, {:e}, {:e}",
-                    name,
-                    tot_rel / size_r as f64,
-                    tot_err / size_e as f64
-                );
-            }
+    
+    for elem in res.into_iter(){
+        if let (nome, Some((avg, abs))) = elem {
+            println!("{}, {}, {}", nome, avg, abs);
         }
     }
+
 }
 
