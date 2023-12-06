f5db1f99394ade275d7c6decf71eaa6afb19aa06
ilgeco <gabriele.magnani@mail.polimi.it>
2023-12-06 14:16:22 +0100

    stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,7 @@
-#![feature(test)]
-extern crate test;
-
 #[macro_use]
 extern crate lazy_static;
-
-
 
 pub mod cli;
 pub mod input_parser;
 pub mod mode;
 pub mod util;
-
-
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,18 @@
-#![feature(test)]
-extern crate test;
-
 #[macro_use]
 extern crate lazy_static;
-
 
 use clap::Parser;
 use cli::pretty_print;
 
 use crate::{cli::Cli, mode::dispatch};
 
-
 mod cli;
 mod input_parser;
 mod mode;
 mod util;
-
-
-
-
-
-
-
-
 
 fn main() {
     let cli = Cli::parse();
     let res = dispatch(cli.command);
     pretty_print(res, cli.ascii);
 }
-
-
